d869da835fde7819be0ae223fe57cd6c58a381c1
Kartik Chauhan <kckartik17@gmail.com>
2019-12-21 20:32:32 +0530

    implemented 7 1 arch

---
+++ Natours/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => res.render("index.html"));
+
+app.listen(3000, () => console.log("Server started on 3000..."));
